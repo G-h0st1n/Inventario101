8ff9f8dbaabca74d794e77267dab6411b43307e9
G-h0st1n <a01645013@tec.mx>
2024-11-22 18:09:02 -0600

    generar historial

---
+++ History.h
@@ -0,0 +1,22 @@
+//
+// Created by frpat on 11/22/2024.
+//
+
+#ifndef HISTORY_H
+#define HISTORY_H
+#include <string>
+#include <list>
+using namespace std;
+
+
+class History {
+private:
+    list<string> records;
+public:
+    void addRecord(const string& record);
+    void showHistory();
+};
+
+
+
+#endif //HISTORY_H
